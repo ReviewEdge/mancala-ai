c4100042e82652b2a4a707603070dd649038b100
Logan Richardson <reviewedge56@gmail.com>
2023-04-30 14:34:34 -0400

    add tester loop

--- src/MancalaGame.java
+++ src/MancalaGame.java
@@ -1,8 +1,80 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
+
+
+public class MancalaGame {
+
+    public static void main(String[] args) {
+        MancalaBoard mancalaGame;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Mancala");
+        System.out.println("Enter Game Method:\n1. Human vs AI\t2. AI vs AI\t");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Enter AI Heuristic: ");
+            E_HEURISTICS heuristics = GetHeuristic(scanner);
+            mancalaGame = new MancalaBoard(E_HEURISTICS.H_HUMAN, heuristics);
+        } else if (choice == 2) {
+            mancalaGame = new MancalaBoard();
+            for (int i = 0; i < e_heuristics.length; i++) {//player 1
+                for (int j = i; j < e_heuristics.length; j++) {//player 2
+
+                    for (int k = 0; k < MATCH_PER_HEURISTIC; k++) {//, trials deterministic, only needs one game, same every time
+                        System.out.println("--- matchup H" + (i+1) + " vs H" + (j+1) + " ---");
+                        mancalaGame = new MancalaBoard(e_heuristics[i], e_heuristics[j]);
+                        mancalaGame.GameLoop();
+
+                        System.out.println();
+                        System.out.println();
+
+                    }
+                    System.out.println();
+
+                }
+            }
+        } else {
+            System.out.println("Not a valid choice");
+            return;
+        }
+    }
+
+
+    public static E_HEURISTICS GetHeuristic(Scanner scanner){
+        PrintHeuristics();
+        int Choice = scanner.nextInt();
+        if(Choice-1 < 0 || Choice-1 >= e_heuristics.length){
+            System.out.println("Invalid option, aborting");
+            System.exit(-1);
+        }
+        return e_heuristics[Choice-1];
+    }
+    public static int BIN_COUNT = 6;
+    public static int INITIAL_STONE_PER_BIN = 4;
+    public static int MAX_DEPTH = 8;
+    public static int INFINITY = Integer.MAX_VALUE;
+    public static int NEG_INFINITY = Integer.MIN_VALUE;
+    public static int MATCH_PER_HEURISTIC = 1;//deterministic, only needs one game, same every time
+
+    static E_HEURISTICS[] e_heuristics = {E_HEURISTICS.H_STORAGE_DIFFERENCE, E_HEURISTICS.H_STONE_AND_STORAGE, E_HEURISTICS.H_CHAIN_MOVES_PRIORITIZE,
+            E_HEURISTICS.H_STONES_CLOSE_TO_MY_STORAGE, E_HEURISTICS.H_CLOSE_TO_WINNING, E_HEURISTICS.H_REDUCE_OPPONENT_POINT};
+
+
+
+    public static void PrintHeuristics(){
+        int Counter = 0;
+        for(E_HEURISTICS eHeuristics : e_heuristics){
+            System.out.print(E_HEURISTICS.ToString(eHeuristics));
+            Counter++;
+            if(Counter%2==0){
+                System.out.println();
+            }else{
+                System.out.print("\t");
+            }
+        }
+    }
+
+
+}
 
 enum E_HEURISTICS{
     H_STORAGE_DIFFERENCE,
@@ -40,68 +112,5 @@ enum E_HEURISTICS{
                 return "Error";
             }
         }
-    }
-}
-
-public class MancalaGame {
-    public static int BIN_COUNT = 6;
-    public static int INITIAL_STONE_PER_BIN = 4;
-    public static int MAX_DEPTH = 10; // Absolute max depth
-    public static int INFINITY = Integer.MAX_VALUE;
-    public static int NEG_INFINITY = Integer.MIN_VALUE;
-    public static int MATCH_PER_HEURISTIC = 10;
-
-    static E_HEURISTICS[] e_heuristics = {E_HEURISTICS.H_STORAGE_DIFFERENCE, E_HEURISTICS.H_STONE_AND_STORAGE, E_HEURISTICS.H_CHAIN_MOVES_PRIORITIZE,
-            E_HEURISTICS.H_STONES_CLOSE_TO_MY_STORAGE, E_HEURISTICS.H_CLOSE_TO_WINNING, E_HEURISTICS.H_REDUCE_OPPONENT_POINT};
-
-
-
-    public static void PrintHeuristics(){
-        int Counter = 0;
-        for(E_HEURISTICS eHeuristics : e_heuristics){
-            System.out.print(E_HEURISTICS.ToString(eHeuristics));
-            Counter++;
-            if(Counter%2==0){
-                System.out.println();
-            }else{
-                System.out.print("\t");
-            }
-        }
-    }
-
-    public static E_HEURISTICS GetHeuristic(Scanner scanner){
-        PrintHeuristics();
-        int Choice = scanner.nextInt();
-        if(Choice-1 < 0 || Choice-1 >= e_heuristics.length){
-            System.out.println("Invalid option, aborting");
-            System.exit(-1);
-        }
-        return e_heuristics[Choice-1];
-    }
-
-    public static void main(String[] args) {
-        int Choice;
-        MancalaBoard mancalaGame;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Mancala");
-        System.out.println("Enter Game Method:\n1. Human vs AI\t2. AI vs AI\t");
-        Choice = scanner.nextInt();
-
-        if(Choice==1){
-            System.out.println("Enter AI Heuristic: ");
-            E_HEURISTICS heuristics = GetHeuristic(scanner);
-            mancalaGame = new MancalaBoard(E_HEURISTICS.H_HUMAN, heuristics);
-        }else if(Choice == 2){
-            System.out.println("Enter First AI Heuristic: ");
-            E_HEURISTICS heuristics1 = GetHeuristic(scanner);
-            System.out.println("Enter Second AI Heuristic: ");
-            E_HEURISTICS heuristics2 = GetHeuristic(scanner);
-            mancalaGame = new MancalaBoard(heuristics1, heuristics2);
-        }else{
-            System.out.println("Not a valid choice");
-            return;
-        }
-
-        mancalaGame.GameLoop();
     }
 }
